ff403dad9a3470074c159c78af5acb0b53ce53ba
Jipxmaster <johnoshirokarita@gmail.com>
2024-11-01 21:02:50 -0500

    Create Burst_test.ino

---
+++ Burst_test.ino
@@ -0,0 +1,14 @@
+extern "C"{
+  void start();
+  void burst();
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  start();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  burst();
+}
